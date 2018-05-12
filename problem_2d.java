d5d1fd5a12e6f75f356ceaa1af90e30114db1dd2
nupur2018 <39194563+nupur2018@users.noreply.github.com>
2018-05-12 20:25:50 +0600

    Add files via upload

---
+++ problem_2d.java
@@ -0,0 +1,9 @@
+
+public class _2d {
+	public static void main(String[] args){
+		String s="Roksana Nupur";  
+		   System.out.println(s.substring(0,6));  
+		   System.out.println(s.substring(7));
+	}
+
+}
